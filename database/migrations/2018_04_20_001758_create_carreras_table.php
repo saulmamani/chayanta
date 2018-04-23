9a7544b6fd79c91b01b345ae6d6bd6fd9ba54d2d
saul <luas0_1@yahoo.es>
2018-04-23 08:37:44 -0400

    migraciones arregladas

--- database/migrations/2018_04_20_001758_create_carreras_table.php
+++ database/migrations/2018_04_20_001758_create_carreras_table.php
@@ -16,6 +16,7 @@ class CreateCarrerasTable extends Migration
         Schema::create('carreras', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre',  100);
+            $table->text('descripcion');
             $table->string('area',  50);
             $table->integer('tiempo');
             $table->string('ruta');
